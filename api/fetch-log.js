58b88158079c2cce9f7086353199db7c47c1f1f3
Tim Pu <shoppy09@gmail.com>
2026-05-18 14:57:47 +0800

    chore: 更新 fetch-log.js 指向 tzlth-hq canonical cron

--- api/fetch-log.js
+++ api/fetch-log.js
@@ -2,10 +2,10 @@ module.exports = (req, res) => {
   res.json({
     exists: true,
     lines: [
-      '[GitHub Actions] 資料由 cron.yml 每日 10:00 / 22:00（台灣時間）自動更新',
-      '[提示] 最新執行記錄請前往：github.com/shoppy09/tzlth-threads-dashboard/actions'
+      '[GitHub Actions] 資料由 tzlth-hq fetch-threads.yml 每日 09:00 / 21:00（台灣時間）自動更新',
+      '[提示] 最新執行記錄請前往：github.com/shoppy09/tzlth-hq/actions/workflows/fetch-threads.yml'
     ],
-    lastSuccess: '自動更新已啟用（GitHub Actions cron）',
+    lastSuccess: '自動更新已啟用（tzlth-hq canonical cron）',
     lastFail: null
   });
 };
